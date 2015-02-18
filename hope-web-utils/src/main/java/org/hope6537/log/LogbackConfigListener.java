3c69df2703d985a56a3a27696489e94009575f67
Zhao Peng <hope6537@qq.com>
2015-02-19 02:47:34 +0800

    [info #3]将整个工程抽象出3个模块，web-utils用于引导web工程和工具类，code-note笔记，hadoop作为hadoop工程基本依赖和工具类，同时hadoop-utils将引用部分web-utils代码

---
+++ hope-web-utils/src/main/java/org/hope6537/log/LogbackConfigListener.java
@@ -0,0 +1,45 @@
+package org.hope6537.log;/*
+package org.hope6537.log;
+
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.joran.JoranConfigurator;
+import ch.qos.logback.core.joran.spi.JoranException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+*/
+/**
+ * LogbackConfigListener
+ *//*
+
+public class LogbackConfigListener implements ServletContextListener {
+
+    private static final Logger logger = LoggerFactory.getLogger(LogbackConfigListener.class);
+
+    private static final String CONFIG_LOCATION = "logbackConfigLocation";
+
+    @Override
+    public void contextInitialized(ServletContextEvent event) {
+        //从web.xml中加载指定文件名的日志配置文件
+        String logbackConfigLocation = event.getServletContext().getInitParameter(CONFIG_LOCATION);
+        String fn = event.getServletContext().getRealPath(logbackConfigLocation);
+        try {
+            LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
+            loggerContext.reset();
+            JoranConfigurator joranConfigurator = new JoranConfigurator();
+            joranConfigurator.setContext(loggerContext);
+            joranConfigurator.doConfigure(fn);
+            logger.debug("loaded slf4j configure file from {}", fn);
+        } catch (JoranException e) {
+            logger.error("can't loading slf4j configure file from " + fn, e);
+        }
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent event) {
+    }
+}
+*/
