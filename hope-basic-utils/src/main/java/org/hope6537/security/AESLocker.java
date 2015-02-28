99870e265e5e31e85d5dfa7363f3b14e63b89e59
Zhao Peng <hope6537@qq.com>
2015-02-28 20:20:19 +0800

    [info #10]AES加密更改，增加混淆

--- hope-basic-utils/src/main/java/org/hope6537/security/AESLocker.java
+++ hope-basic-utils/src/main/java/org/hope6537/security/AESLocker.java
@@ -1,129 +1,94 @@
 package org.hope6537.security;
 
+import org.junit.Test;
+import sun.misc.BASE64Decoder;
+
 import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
+
+import static org.junit.Assert.assertEquals;
 
 public class AESLocker {
 
-
-    private static String Decrypt(String sSrc, String sKey) {
-        try {
-            // 判断Key是否正确
-            if (sKey == null) {
-                // sKey = "Hope6537JiChuang";
-                sKey = "2014-11-09 16:18";
-            }
-            // 判断Key是否为16位
-            if (sKey.length() != 16) {
-                System.out.print("Key长度不是16位");
-                return null;
-            }
-
-            byte[] raw = sKey.getBytes("ASCII");
-            SecretKeySpec skeySpec = new SecretKeySpec(raw, "AES");
-            Cipher cipher = Cipher.getInstance("AES");
-            cipher.init(Cipher.DECRYPT_MODE, skeySpec);
-            byte[] encrypted1 = hex2byte(sSrc);
-            try {
-                byte[] original = cipher.doFinal(encrypted1);
-                String originalString = new String(original);
-                return originalString;
-            } catch (Exception e) {
-                System.out.println(e.toString());
-                return null;
-            }
-        } catch (Exception ex) {
-            System.out.println(ex.toString());
-            return null;
-        }
+    @Test
+    public void testAES() {
+        String s1 = encrypt("sdqwdqwdqwdqw");
+        System.out.println(s1);
+        String s2 = decrypt(s1);
+        assertEquals("sdqwdqwdqwdqw", s2);
     }
 
-    /**
-     * <p>Describe: 对字符串进行解密操作</p>
-     * <p>Using: </p>
-     * <p>How To Work: </p>
-     * <p>DevelopedTime: 2014年10月12日下午3:20:35 </p>
-     * <p>Author:Hope6537</p>
-     *
-     * @param sSrc
-     * @param sKey
-     * @return
-     * @throws Exception
-     * @see
-     */
-    public static String Decrypt(String sSrc) {
-        return Decrypt(sSrc, null);
-    }
-
-    private static String Encrypt(String sSrc, String sKey) {
-        if (sKey == null) {
-            // sKey = "Hope6537JiChuang";
-            sKey = "2014-11-09 16:18";
-        }
-        // 判断Key是否为16位
-        if (sKey.length() != 16) {
-            System.out.print("Key长度不是16位");
-            return null;
-        }
+    public static String encrypt(String data) {
         try {
-            byte[] raw = sKey.getBytes("ASCII");
-            SecretKeySpec skeySpec = new SecretKeySpec(raw, "AES");
-            Cipher cipher = Cipher.getInstance("AES");
-            cipher.init(Cipher.ENCRYPT_MODE, skeySpec);
-            byte[] encrypted = cipher.doFinal(sSrc.getBytes());
-            return byte2hex(encrypted).toLowerCase();
+            return encrypt(data, "Hope6537JiChuang");
         } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
-
     }
 
-    /**
-     * <p>Describe: 对字符串进行加密操作</p>
-     * <p>Using: </p>
-     * <p>How To Work: </p>
-     * <p>DevelopedTime: 2014年10月12日下午3:20:15 </p>
-     * <p>Author:Hope6537</p>
-     *
-     * @param sSrc
-     * @param sKey
-     * @return
-     * @throws Exception
-     * @see
-     */
-    public static String Encrypt(String sSrc) {
-        return Encrypt(sSrc, null);
-    }
-
-    private static byte[] hex2byte(String strhex) {
-        if (strhex == null) {
+    public static String decrypt(String data) {
+        try {
+            return decrypt(data, "Hope6537JiChuang");
+        } catch (Exception e) {
+            e.printStackTrace();
             return null;
         }
-        int l = strhex.length();
-        if (l % 2 == 1) {
-            return null;
-        }
-        byte[] b = new byte[l / 2];
-        for (int i = 0; i != l / 2; i++) {
-            b[i] = (byte) Integer.parseInt(strhex.substring(i * 2, i * 2 + 2),
-                    16);
-        }
-        return b;
     }
 
-    private static String byte2hex(byte[] b) {
-        String hs = "";
-        String stmp = "";
-        for (int n = 0; n < b.length; n++) {
-            stmp = (Integer.toHexString(b[n] & 0XFF));
-            if (stmp.length() == 1) {
-                hs = hs + "0" + stmp;
-            } else {
-                hs = hs + stmp;
+
+    private static String encrypt(String data, String key) throws Exception {
+        try {
+            if (key == null || key.length() != 16) {
+                throw new RuntimeException("key error");
             }
+            String iv = "1234567812345678";
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            int blockSize = cipher.getBlockSize();
+
+            byte[] dataBytes = data.getBytes();
+            int plaintextLength = dataBytes.length;
+            if (plaintextLength % blockSize != 0) {
+                plaintextLength = plaintextLength + (blockSize - (plaintextLength % blockSize));
+            }
+
+            byte[] plaintext = new byte[plaintextLength];
+            System.arraycopy(dataBytes, 0, plaintext, 0, dataBytes.length);
+
+            SecretKeySpec keyspec = new SecretKeySpec(key.getBytes(), "AES");
+            IvParameterSpec ivspec = new IvParameterSpec(iv.getBytes());
+
+            cipher.init(Cipher.ENCRYPT_MODE, keyspec, ivspec);
+            byte[] encrypted = cipher.doFinal(plaintext);
+
+            return new sun.misc.BASE64Encoder().encode(encrypted);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
         }
-        return hs.toUpperCase();
     }
 
+    private static String decrypt(String data, String key) throws Exception {
+        try {
+            if (key == null || key.length() != 16) {
+                throw new RuntimeException("key error");
+            }
+            String iv = "1234567812345678";
+            byte[] encrypted1 = new BASE64Decoder().decodeBuffer(data);
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            SecretKeySpec keyspec = new SecretKeySpec(key.getBytes(), "AES");
+            IvParameterSpec ivspec = new IvParameterSpec(iv.getBytes());
+
+            cipher.init(Cipher.DECRYPT_MODE, keyspec, ivspec);
+
+            byte[] original = cipher.doFinal(encrypted1);
+            String originalString = new String(original).trim();
+            return originalString;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
