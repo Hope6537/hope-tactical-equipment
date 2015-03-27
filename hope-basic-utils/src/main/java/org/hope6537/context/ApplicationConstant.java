a75abb7e0b81e0d31dc665cc1467487cad9e965e
Zhao Peng <hope6537@qq.com>
2015-03-27 13:12:21 +0800

    [info #20]迭代版本

--- hope-basic-utils/src/main/java/org/hope6537/context/ApplicationConstant.java
+++ hope-basic-utils/src/main/java/org/hope6537/context/ApplicationConstant.java
@@ -29,18 +29,40 @@ public class ApplicationConstant {
     public static final String STATUS_DIE = "不可用";
     protected Logger logger = LoggerFactory.getLogger(getClass());
 
-    public static boolean notNull(Object o) {
-        if (o instanceof String) {
-            return o != null && !((String) o).isEmpty();
+    public static boolean notNull(Object obj) {
+        boolean res = obj != null;
+        if (obj instanceof String) {
+            return obj != null && !((String) obj).isEmpty();
         }
-        if (o instanceof Collection) {
-            return o != null && !((Collection) o).isEmpty();
+        if (obj instanceof Collection) {
+            return obj != null && !((Collection) obj).isEmpty();
         }
-        return o != null;
+        return res;
     }
 
-    public static boolean isNull(Object o) {
-        return !notNull(o);
+    public static boolean notNull(Object... objs) {
+        boolean res = objs != null;
+        if (!res) {
+            return false;
+        }
+        for (Object o : objs) {
+            if (o instanceof String) {
+                res = res && o != null && !((String) o).isEmpty();
+            }
+            if (o instanceof Collection) {
+                res = res && o != null && !((Collection) o).isEmpty();
+            }
+            res = res && o != null;
+        }
+        return res;
+    }
+
+    public static boolean isNull(Object obj) {
+        return !notNull(obj);
+    }
+
+    public static boolean isNull(Object... objs) {
+        return !notNull(objs);
     }
 
 }
