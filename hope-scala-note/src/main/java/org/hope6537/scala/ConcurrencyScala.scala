e1c15a5c20339d0606511701fc24678320230fe8
Zhao Peng <hope6537@qq.com>
2015-04-12 17:27:26 +0800

    [extend #32]基于Java的Scala并发机制

--- hope-scala-note/src/main/java/org/hope6537/scala/ConcurrencyScala.scala
+++ hope-scala-note/src/main/java/org/hope6537/scala/ConcurrencyScala.scala
@@ -1,9 +1,141 @@
 package org.hope6537.scala
 
+import java.net.{ServerSocket, Socket}
+import java.util.concurrent._
+
+import akka.actor.{Actor, ActorSystem, Props}
+import akka.testkit.CallingThreadDispatcher
+import com.typesafe.config.ConfigFactory
+
+
 /**
  * Scala并发
  */
 class ConcurrencyScala {
+
+  def basicMode() = {
+
+    /**
+     * 基本的runnable线程模式
+     */
+    val hello = new Thread(new Runnable {
+      override def run() = println("Runnable")
+    })
+    hello.start()
+
+
+    /**
+     * 创建一个网络请求响应服务
+     */
+    class NetWorkService(port: Int, poolSize: Int) extends Runnable {
+      val serverSocket = new ServerSocket(port)
+
+      override def run(): Unit = {
+        val socket = serverSocket.accept()
+
+        /**
+         * 但是在单位时间下只能有一个请求可以被响应
+         */
+        //new Handler(socket).run()
+
+        /**
+         * 但是我们可以把每个请求放入一个线程中处理
+         */
+        new Thread(new Handler(socket)).start()
+
+      }
+    }
+    /**
+     * 请求控制，打印输出信息
+     */
+    class Handler(socket: Socket) extends Runnable {
+
+      def message = (Thread.currentThread().getName + "\n").getBytes("UTF-8")
+
+      override def run(): Unit = {
+        socket.getOutputStream.write(message)
+        socket.getOutputStream.close()
+        println(Thread.currentThread().getName)
+      }
+    }
+
+    new NetWorkService(2020, 2).run()
+  }
+
+  /**
+   * 用我们最熟悉的执行器进行操作~
+   */
+  def execMode() = {
+    /**
+     * 我们可以重写刚才那个阻塞式服务器的请求
+     */
+    class NetWorkService(port: Int, poolSize: Int) extends Runnable {
+      val serverSocket = new ServerSocket(port)
+      /**
+       * 设置线程池
+       */
+      val pool: ExecutorService = Executors.newFixedThreadPool(poolSize)
+
+      override def run(): Unit = {
+        try {
+          while (true /*&& !Thread.interrupted()*/ ) {
+            val socket = serverSocket.accept()
+            pool.execute(new Handler(socket))
+          }
+        } finally {
+          //将不再容纳新线程
+          pool.shutdown()
+        }
+      }
+
+    }
+    /**
+     * 请求控制，打印输出信息
+     */
+    class Handler(socket: Socket) extends Runnable {
+
+      def message = (Thread.currentThread().getName + "\n").getBytes("UTF-8")
+
+      override def run(): Unit = {
+        socket.getOutputStream.write(message)
+        socket.getOutputStream.close()
+        println(message)
+      }
+    }
+
+    new NetWorkService(2021, 2).run()
+  }
+
+  def ajaxFutures() = {
+    val future = new FutureTask[String](new Callable[String] {
+      override def call(): String = "return String"
+    })
+    val exec: ExecutorService = Executors.newCachedThreadPool()
+    exec.execute(future)
+    var blockingResult: String = null
+    println("->" + blockingResult)
+    blockingResult = future.get()
+    println("->" + blockingResult)
+  }
+
+  /**
+   * 线程安全的方法在这里变成了两种
+   * 1、synchronized代码块
+   * 2、原子、瞬时共享变量
+   */
+  def threadSafe() = {
+
+
+    class Person(/*标记瞬时@volatile*/ var name: String) {
+      def set(name: String): Unit = {
+        this.synchronized {
+          this.name = name
+        }
+      }
+    }
+
+
+  }
 
   def basicActor() = {
 
@@ -27,8 +159,6 @@ class ConcurrencyScala {
    * actor-akka机理
    */
   def actorSystem() = {
-    import akka.actor.{Actor, ActorSystem, Props}
-    import akka.testkit.CallingThreadDispatcher
     implicit val system = ActorSystem()
     class EchoServer(name: String) extends Actor {
       def receive = {
@@ -89,8 +219,6 @@ class ConcurrencyScala {
    * 远程传输
    */
   def remote() = {
-    import akka.actor.{Actor, Props}
-    import com.typesafe.config.ConfigFactory
 
     //重载
     implicit val system = akka.actor.ActorSystem("RemoteSystem", ConfigFactory.load.getConfig("remote"))
@@ -121,5 +249,8 @@ object ConcurrencyScalaClient extends App {
   obj.actorSystem()*/
   //obj.parallelsCollection()
   //obj.wordCount()
-  obj.remote()
+  //obj.remote()
+  //obj.basicMode()
+  //obj.execMode()
+  obj.ajaxFutures()
 }
