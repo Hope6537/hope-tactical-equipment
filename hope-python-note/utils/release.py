4d3e76e2bdfea0186273af93997f0bbe6972b162
Zhao Peng <hope6537@qq.com>
2015-05-12 20:10:49 +0800

    [info #38]Python IO工具

---
+++ hope-python-note/utils/release.py
@@ -0,0 +1,27 @@
+# encoding: utf-8
+
+"""Release information about Marrow IO."""
+
+from collections import namedtuple
+
+
+__all__ = ['version_info', 'version']
+
+
+version_info = namedtuple('version_info', ('major', 'minor', 'micro', 'releaselevel', 'serial'))(1, 1, 0, 'final', 0)
+
+version = ".".join([str(i) for i in version_info[:3]]) + ((version_info.releaselevel[0] + str(version_info.serial)) if version_info.releaselevel != 'final' else '')
+
+
+name = "marrow.io"
+version = "1.1.0"
+release = "1.1"
+
+summary = "Standalone versions of the Tornado IOLoop and IOStream."
+description = """"""
+author = "Facebook"
+email = ""
+url = "http://github.com/pulp/marrow.io"
+download_url = "http://github.com/pulp/marrow.io/downloads"
+copyright = "2009, Facebook"
+license = "Apache 2.0"
