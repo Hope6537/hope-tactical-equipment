246535de3b7f7311f32f4242527cd772357c71d5
Zhao Peng <hope6537@qq.com>
2015-08-20 23:42:31 +0800

    [extends #37]eHentai

--- hope-python-note/single/eHentai.py
+++ hope-python-note/single/eHentai.py
@@ -1,9 +1,11 @@
 # encoding:UTF-8
 import cookielib
-import io
 import os
 import urllib
 import urllib2
+
+from pyquery import PyQuery as pq
+
 
 __author__ = 'Hope6537'
 
@@ -11,8 +13,6 @@ wget = "wget "
 https = "https://"
 dir = "D:/eHentai/"
 urlFile = open('D:/url.txt', 'a')
-from pyquery import PyQuery as pq
-
 site = "http://lofi.e-hentai.org/"
 cj = cookielib.CookieJar()
 opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
@@ -23,18 +23,22 @@ urllib2.install_opener(opener)
 d = pq(url='http://lofi.e-hentai.org/?f_search=chinese&f_apply=Search')
 data = d('.ig')
 for i in data:
-    doc = pq(i).find("a").attr("href")[-10:-1]
+    doc = pq(i).find("a").attr("href")[-10:-1] + '/'
     os.mkdir(dir + doc)
+    print("mkdir on " + (dir + doc))
     contentLink = pq(url=pq(i).find("a").attr("href")).find("a:first").attr("href")
+    print("contentLink is " + contentLink)
     preLink = ""
     nextLink = "this"
     while preLink != nextLink:
         nextLink = pq(contentLink).find("#sd").find("a").attr("href")
         imgLink = pq(contentLink).find("#sm").attr("src")
+        print("writing " + (wget + imgLink))
         urlFile.write(wget + imgLink + ";\n")
+        print("download " + (dir + doc + imgLink[-8:]))
         urllib.urlretrieve(imgLink, dir + doc + imgLink[-8:])
+        print("download complete")
         urlFile.flush()
-        print(nextLink)
         preLink = contentLink
         contentLink = nextLink
 urlFile.close()
