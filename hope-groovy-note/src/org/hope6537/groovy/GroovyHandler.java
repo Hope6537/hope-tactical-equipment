3e0181580b1bdd786554010b5efaaf3d6f3490a9
Zhao Peng <hope6537@qq.com>
2015-10-21 22:45:12 +0800

    [info #43]groovy env

---
+++ hope-groovy-note/src/org/hope6537/groovy/GroovyHandler.java
@@ -0,0 +1,14 @@
+package org.hope6537.groovy;
+
+/**
+ * Created by hope6537 on 15/10/21.
+ */
+public class GroovyHandler {
+
+    public static void main(String[] args) {
+
+        GroovyTest groovyTest = new GroovyTest();
+        System.out.println(groovyTest.numberCastAdd(1));
+
+    }
+}
---
+++ hope-groovy-note/src/org/hope6537/groovy/GroovyTest.groovy
@@ -0,0 +1,10 @@
+package org.hope6537.groovy;
+/**
+ * Created by hope6537 on 15/10/21.
+ */
+class GroovyTest {
+
+    def numberCastAdd(int i){
+        return i+1;
+    }
+}
