1efc0a3808bb49802cb9c77b3c4dd448cb42cfff
hope6537 <hope6537@qq.com>
2016-05-12 07:11:42 +0800

    [add svn digger]

--- hope-python-script/network/web_attack/content_bruter.py
+++ hope-python-script/network/web_attack/content_bruter.py
@@ -6,7 +6,7 @@ import urllib2
 
 threads = 5
 target_url = "http://testphp.vulnweb.com"
-wordlist_file = "/tmp/all.txt"  # from SVNDigger
+wordlist_file = "./SVNDigger/all.txt"  # from SVNDigger
 resume = None
 user_agent = "Mozilla/5.0 (X11; Linux x86_64; rv:19.0) Gecko/20100101 Firefox/19.0"
 
@@ -47,24 +47,24 @@ def dir_bruter(extensions=None):
 
         attempt_list = []
 
-        # check if there is a file extension if not
-        # it's a directory path we're bruting
+        # 检查是否有文件拓展名,如果没有,那么就是我们要暴力破解的路径
         if "." not in attempt:
             attempt_list.append("/%s/" % attempt)
         else:
             attempt_list.append("/%s" % attempt)
 
-        # if we want to bruteforce extensions
+        # 如果要暴力拓展,此时使用字典字符对添加每一个想要测试的文件拓展名进行测试
         if extensions:
             for extension in extensions:
                 attempt_list.append("/%s%s" % (attempt, extension))
 
-        # iterate over our list of attempts        
+        # 迭代尝试列表
         for brute in attempt_list:
 
             url = "%s%s" % (target_url, urllib.quote(brute))
 
             try:
+                # 添加User-Agent测试远程服务器
                 headers = {}
                 headers["User-Agent"] = user_agent
                 r = urllib2.Request(url, headers=headers)
