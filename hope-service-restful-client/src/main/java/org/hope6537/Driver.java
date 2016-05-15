82d9493b10d10551fd576efa112a1d159ea1ce41
hope6537 <hope6537@qq.com>
2016-05-16 00:25:26 +0800

    [update controller]

--- hope-service-restful-client/src/main/java/org/hope6537/Driver.java
+++ hope-service-restful-client/src/main/java/org/hope6537/Driver.java
@@ -1,9 +1,7 @@
 package org.hope6537;
 
-import org.hope6537.controller.CategoryController;
 import org.hope6537.controller.LiveTemplateController;
 import org.hope6537.controller.LockerController;
-import org.hope6537.controller.WelcomeController;
 import org.springframework.boot.SpringApplication;
 
 /**
@@ -12,7 +10,7 @@ import org.springframework.boot.SpringApplication;
 public class Driver {
 
     public static void main(String[] args) {
-        Class[] classes = new Class[]{LockerController.class, LiveTemplateController.class, WelcomeController.class, CategoryController.class};
+        Class[] classes = new Class[]{LockerController.class, LiveTemplateController.class};
         SpringApplication.run(classes, args);
     }
 
