2426edf612681301b3185ff6ac67b8700742c2b3
hope6537 <hope6537@qq.com>
2016-05-17 12:32:08 +0800

    [fetch from master]

---
+++ hope-service-rpc-deploy/src/main/java/DubboDriver.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+
+/**
+ * Dubbo本地服务驱动
+ * Created by hope6537 on 16/3/13.
+ */
+public class DubboDriver {
+
+    public static void main(String[] args) throws IOException {
+        com.alibaba.dubbo.container.Main.main(args);
+    }
+
+}
